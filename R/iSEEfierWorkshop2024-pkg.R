d3ac522121c294aaec69e26e4ad5f80865128a6d
Najla Abassi <abassi.nejla96@gmail.com>
2024-11-07 14:30:26 +0100

    add pkg

---
+++ R/iSEEfierWorkshop2024-pkg.R
@@ -0,0 +1,10 @@
+#' iSEEfierWorkshop2024: Learn how to use iSEE/iSEEfier to visualize omics data
+#'
+#' @import iSEE
+#' @import iSEEfier
+#' @importFrom SingleCellExperiment SingleCellExperiment
+#'
+#' @name iSEEfierWorkshop2024-pkg
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
